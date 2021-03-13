9fce3308f395c95b749ca0ca658c577e2e99b003
Preetham <sthelluri1@student.gsu.edu>
2021-03-13 17:35:27 -0500

    Added a more sophisticated version of the program with modified variable names

--- General_Programs/fileextract.py
+++ General_Programs/fileextract.py
@@ -1,23 +1,27 @@
 import sys
-def return_column_vals_from_csv_file(fname, n):
+def return_column_vals_from_csv_file(fname, input_column):
     f = open(fname, "r")
-    z = f.readline()
+    column_list = f.readline()
     data = f.readlines()
-    a = z.split(",")
+    columns = column_list.split(",")
     h = "a"
     line1 = []
-    for quan in a:
-        line1.append(quan.lstrip())
-    if n in line1:
-        h = line1.index(n)
-    elif n+'\n' in line1:
-        h = line1.index(n+'\n')
+    output_list = []
+    for col_name in columns:
+        line1.append(col_name.lstrip())
+    if input_column in line1:
+        h = line1.index(input_column)
+    elif input_column+'\n' in line1:
+        h = line1.index(input_column+'\n')
     else:
         print("The element is invalid, try entering a different one.")
-        return
+        return("")
     for elem in data:
         ind_data = elem.split(",")
-        print((ind_data[h].replace("\n", "")).lstrip())
+        #print((ind_data[h].replace("\n", "")).lstrip())
+        output_list.append((ind_data[h].replace("\n", "")).lstrip())
+    return(output_list)
 if __name__ == "__main__":
-    n = input("Enter the column name: ")
-    return_column_vals_from_csv_file(sys.argv[1], n)
+    inp_col = input("Enter the column name: ")
+    resultant = return_column_vals_from_csv_file(sys.argv[1], inp_col)
+    print(resultant)
